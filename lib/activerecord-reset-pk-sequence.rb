295cae26c2dc0c3c8fc5db26222d020796c06f05
Thomas Kienlen <thomas.kienlen@lafourmi-immo.com>
2012-10-11 09:10:37 +0200

    added delete_all_and_reset method

--- lib/activerecord-reset-pk-sequence.rb
+++ lib/activerecord-reset-pk-sequence.rb
@@ -14,5 +14,9 @@ module ActiveRecord
         raise "Task not implemented for this DB adapter"
       end 
     end
+    def self.delete_all_and_reset
+      delete_all
+      reset_pk_sequence
+    end
   end
 end
