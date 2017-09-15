470ec0b2e97ce3b12e82e195491673e78a4c0750
Paul Philippov <themactep@gmail.com>
2017-09-15 14:10:11 -0400

    Update activerecord-reset-pk-sequence.rb

--- lib/activerecord-reset-pk-sequence.rb
+++ lib/activerecord-reset-pk-sequence.rb
@@ -9,8 +9,8 @@ module ActiveRecord
         update_seq_sql = "UPDATE sqlite_sequence SET seq = #{new_max} WHERE name = '#{table_name}';"
         ActiveRecord::Base.connection.execute(update_seq_sql)
       when 'Mysql', 'Mysql2'
-        new_max = maximum(primary_key) + 1 || 1
-        update_seq_sql = "ALTER TABLE '#{table_name}' AUTO_INCREMENT = #{new_max};"
+        new_max = maximum(primary_key).to_i + 1
+        update_seq_sql = "ALTER TABLE `#{table_name}` AUTO_INCREMENT = #{new_max};"
         ActiveRecord::Base.connection.execute(update_seq_sql)
       when 'PostgreSQL'
         ActiveRecord::Base.connection.reset_pk_sequence!(table_name)
